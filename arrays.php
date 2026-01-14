1b7c2001de55229c8f606211c248618b7d7daa87
FranklynSantos <franklynsantos451@gmail.com>
2026-01-14 09:00:22 -0300

    debugging

--- arrays.php
+++ arrays.php
@@ -1,6 +1,15 @@
 <?php
 // arrays
 
-$data = ['Franklyn','Joe','Marie', true, 34];
+$data = [1=>'Franklyn',3=>'Joe',2=>'Marie', 5=> true, 6=> 34];
 
 echo $data[1];
+
+// adding at the final
+$data[] = ['Yo'] ;
+
+
+// adding at the beginning
+// array_unshift($data, 'Initial');
+
+print_r($data);
