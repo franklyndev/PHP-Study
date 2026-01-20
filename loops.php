6a030af0844520f1f6c54c60ad188150b1b0eb23
FranklynSantos <franklynsantos451@gmail.com>
2026-01-20 14:57:30 -0300

    loops

--- loops.php
+++ loops.php
@@ -29,3 +29,34 @@ do{
     echo $names[$i] . "\n";
     $i++; 
 }while ($i < count($names));
+
+// foreach
+echo "\n";
+echo "FOREACH\n------------\n";
+
+foreach ($names as $key => $name) {
+    echo $key . " => $name \n";
+}
+
+
+// Continue
+echo "\n";
+echo "CONTINUE\n------------\n";
+
+for ($i = 0; $i < count($names); $i++){
+    if ($i === 1){
+        continue;
+    }
+    echo $names[$i], "\n";
+}
+
+// Break
+echo "\n";
+echo "BREAK\n------------\n";
+
+for ($i = 0; $i < count($names); $i++){
+    if ($i === 1){
+        break;
+    }
+    echo $names[$i], "\n";
+}
