53229d4604ececf85d053d2236d7fbe573d3b93d
FranklynSantos <franklynsantos451@gmail.com>
2026-01-20 21:11:26 -0300

    functions

--- functions.php
+++ functions.php
@@ -2,15 +2,55 @@
 
 // functions
 
-function connection(){
-    $pdo = new PDO('mysql:host=localhost;dbname=books', 'root', '');
-    return $pdo;
-}
+# Calculate AVERAGE
+$numbers = [10, 20, 30, 40, 50];
 
 
-function getData(){
-    $conn = connection();
-    $query = $conn->query('select * from users');
-    $query->execute();
-    return $query->fetchAll();
+function calculateAverage($numbers){
+    $sum = 0;
+    foreach ($numbers as $number) {
+        $sum += $number;
+    }
+    return $sum / count($numbers); //output: 30
 }
+
+echo "Average: ",calculateAverage($numbers);
+
+echo "\n";
+# Max Number
+
+
+function getMax($numbers){
+    $max = 0;
+    foreach ($numbers as $number) {
+        if ($number > $max){
+            $max = $number;
+        }
+    }
+
+    return $max;
+}
+
+echo "Max: ",getMax($numbers); // output: 50
+
+echo "\n";
+# Counting Even Numbers
+
+function countingEvenNumbers($numbers){
+    $even = 0;
+    foreach ($numbers as $number){
+        if ($number % 2 == 0){
+            $even += 1;
+        }
+    }
+    return $even;
+}
+
+echo "Evens: ",countingEvenNumbers($numbers); // output: 5
+
+
+
+
+
+
+
