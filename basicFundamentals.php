bfdc34bfb99618494e388022fcb85d7b189e5dce
FranklynSantos <franklynsantos451@gmail.com>
2026-01-26 15:43:42 -0300

    Applying PHP in HTML & JS

---
+++ basicFundamentals.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Fundamentals</title>
+    <style>
+        body {
+            background-color: blue;
+        }
+    </style>
+</head>
+<body>
+    
+<?php
+    echo "Hello World! <h3>Interpreting<h3/>\n ";
+    echo "<i>This is my first code mescling PHP, HTML and CSS<i/>";
+?>
+
+</body>
+</html>
