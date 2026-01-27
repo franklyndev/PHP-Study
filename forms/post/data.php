379a0ff0ea9599e94dc3801c3f313942f79c312b
FranklynSantos <franklynsantos451@gmail.com>
2026-01-27 15:36:16 -0300

    How  array works

---
+++ forms/post/data.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$name = $_POST['name'];
+
+$email = $_POST['email'];
+
+echo "Your name is $name and your email is $email <br>";
+
+var_dump($_POST);
+
+?>
---
+++ forms/post/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="data.php" method="POST">
+        Name: <input type="text" name="name">
+        Email: <input type="text" name="email">
+        <button type="submit">submit</button>
+    </form>
+
+</body>
+</html>
