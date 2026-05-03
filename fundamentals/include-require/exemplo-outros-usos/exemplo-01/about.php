e71fd67d48e718549a9760ac81af7e88e1a99a62
FranklynSantos <franklynsantos451@gmail.com>
2026-05-03 17:06:21 -0300

    Example page without Include and Require
    this isnt dynamic

---
+++ fundamentals/include-require/exemplo-outros-usos/exemplo-01/about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 1</title>
+</head>
+<body>
+
+    <nav>
+        <a href="index.php">Home</a> |
+        <a href="about.php">About</a> |
+        <a href="contact.php">Contact</a> |
+    </nav>
+    <h1>About</h1>
+    <hr>
+    <p>This page shows About</p>
+    
+</body>
+</html>
---
+++ fundamentals/include-require/exemplo-outros-usos/exemplo-01/contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 1</title>
+</head>
+<body>
+
+    <nav>
+        <a href="index.php">Home</a> |
+        <a href="about.php">About</a> |
+        <a href="contact.php">Contact</a> |
+    </nav>
+    <h1>Contact</h1>
+    <hr>
+    <p>This page shows my Contact</p>
+
+    <a href="http://github.com/franklyndev"></a>
+    
+</body>
+</html>
