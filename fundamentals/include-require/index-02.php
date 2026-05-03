ce0583823c032dbe324fe9cffbb8b3bed4b5bac8
FranklynSantos <franklynsantos451@gmail.com>
2026-05-03 16:45:12 -0300

    Include & Require
    I have learned how does work these Key-Words

---
+++ fundamentals/include-require/index-02.php
@@ -0,0 +1,10 @@
+<?php
+
+// INCLUDE e REQUIRE
+
+// -------------------------------------------------
+// REQUIRE
+
+require 'script.php';
+require 'outro.php'; // o ficheiro não existe. Vai aparecer um erro
+require 'script.php';
---
+++ fundamentals/include-require/script.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'ola,' . ' vindo de fora do arquivo principal';
