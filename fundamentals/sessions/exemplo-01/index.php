fce7ddb7c988c02afa80625c85b4f35edf091bf0
FranklynSantos <franklynsantos451@gmail.com>
2026-05-03 18:39:01 -0300

    Uso das Sessions no PHP e como funcionam

---
+++ fundamentals/sessions/exemplo-01/index.php
@@ -0,0 +1,38 @@
+<?php 
+
+// todos os scripts devem ter o inicio de sessao, 
+// antes de qualquer output no PHP
+session_start();
+
+// o valor de $nome e $sobrenome vai ser definido tendo
+// em atencao a existencia ou nao das variaveis na super global $_SESSION
+$nome = $_SESSION['nome'] ?? '-';
+$sobrenome = $_SESSION['sobrenome'] ?? '-';
+
+// $_SESSION - contem uma colecao, trata-se de um array de todas as veriaveis que estao na sessao
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php require_once 'nav.php'?>
+
+    <hr>
+    <h2>Exercicio com sessoes de php</h2>
+
+    <h3>Valor da variavel 'nome'</h3>
+    <h1><?= $nome ?></strong></h1>
+
+    <h3>Valor da variavel 'sobrenome':</h3>
+    <h1><?= $sobrenome ?></h1>
+
+
+</body>
+</html>
